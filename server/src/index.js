65702480377b5c4cfd3e563f53c92a38f5b6f403
Munir Mahmud <munirmahmud6@gmail.com>
2022-06-01 06:31:04 +0600

    Error handler added

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,21 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const userRouter = require("./routes/userRoutes");
-const dotenv = require("dotenv");
-
-dotenv.config();
+const morgan = require("morgan");
+const { errorHandler } = require("./middlewares/errorHandler");
+require("express-async-errors");
 
 require("./db");
 
 const PORT = 5000;
 
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use("/api/user", userRouter);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running http://localhost:${PORT}`));
